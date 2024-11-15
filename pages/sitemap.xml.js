35ad6f8df616fad9d31d250bfdb44a2f6bf75189
Jason Grammer <grammerjasonl@gmail.com>
2024-11-14 21:25:21 -0600

    add sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,20 @@
+// pages/sitemap.xml.js
+import { getServerSideProps } from 'next';
+
+export const getServerSideProps = async ({ res }) => {
+  const baseUrl = 'https://spmpsolutions.com';
+  const pages = ["/", "/about", "/services", "/contact", "/commercial", "/homeWash", "/roof", "/gutter", "/window", "/concrete", "/paint", "/faq"]; // Replace with dynamic data
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${pages.map(url => `<url><loc>${baseUrl}${url}</loc></url>`).join('')}
+  </urlset>`;
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+
+  return { props: {} };
+};
+
+export default function Sitemap() {}
