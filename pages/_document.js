540ced950499a8b89845f50b13cd605de4a1d354
SPMP238 <samsonitepaintingandmore@gmail.com>
2025-01-13 00:19:37 -0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,84 +1,43 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en" id="#top">
-      <Head>
-        {/* Meta Tags for SEO and Local Optimization */}
-        <meta name="author" content="Samsonite Painting & More" />
-        <meta name="keywords" content="
-          pressure washing, roof cleaning, gutter cleaning, painting services, algae removal, moss cleaning,
-          roof maintenance, concrete cleaning, graffiti cleaning, window cleaning, Little Rock, Benton, Bryant,
-          Searcy, Conway, Arkansas, Pressure Washing Little Rock, Roof Cleaning Little Rock, Gutter Cleaning Little Rock, 
-          Commercial Pressure Washing Little Rock, Power Washing Little Rock, Roof Cleaning near me, Pressure Washing near me,
-          Roof Cleaning near me, Concrete Pressure Washing Little Rock, Graffiti Removal Little Rock, Window Cleaning Little Rock" />
-        
-        <meta 
-          name="description" 
-          content="Samsonite Painting & More offers expert pressure washing, roof cleaning, gutter cleaning, and more in Little Rock, AR, and surrounding areas. Get professional service for residential and commercial properties." 
-        />
-
-        {/* Social Media Meta Tags */}
-        <meta property="og:site_name" content="Samsonite Painting & More" />
-        <meta property="og:url" content="https://spmpsolutions.com/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Pressure Washing & Painting Services in Little Rock, AR | Samsonite Painting & More" />
-        <meta property="og:description" content="We provide top-notch pressure washing, roof cleaning, and painting services in Little Rock, Benton, Bryant, Conway, and surrounding areas. Call today for a free estimate!" />
-        <meta property="og:image" content="https://spmpsolutions.com/assets/img/logo.png" />
-
-        {/* Structured Data (Schema Markup for Local Business) */}
-        <script
-          type="application/ld+json"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "LocalBusiness",
-              "name": "Samsonite Painting & More",
-              "address": {
-                "@type": "PostalAddress",
-                "addressLocality": "Little Rock",
-                "addressRegion": "AR",
-                "postalCode": "72201"
-                // Removed streetAddress to protect privacy
-              },
-              "telephone": "+1-501-533-9213",
-              "url": "https://spmpsolutions.com",
-              "image": "https://spmpsolutions.com/assets/img/logo.png",
-              "priceRange": "$$$",
-              "openingHours": [
-                "Mo-Fr 08:00-18:00",
-                "Sa 09:00-14:00"
-              ],
-              "description": "Samsonite Painting & More provides professional pressure washing, roof cleaning, gutter cleaning, and painting services to Little Rock and surrounding areas."
-            }),
-          }}
-        />
-
-        {/* Canonical URL */}
-        <link rel="canonical" href="https://spmpsolutions.com/" />
-
-        {/* Favicon */}
-        <link rel="shortcut icon" href="/assets/img/faviconSam.png" />
-
-        {/* Preconnect to Fonts */}
-        <link rel="preconnect" href="https://fonts.bunny.net" />
-        <link 
-          href="https://fonts.bunny.net/css?family=dm-sans:400,400i,500,500i,700,700i|poppins:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i" 
-          rel="stylesheet" 
-        />
-
-        {/* Robots Meta Tag (Ensure pages are crawled) */}
-        <meta name="robots" content="index, follow" />
-
-      </Head>
-
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
-
-    )
-}
+    return (
+        <Html lang="en" id="#top">
+            <Head>
+            <meta name="author" content="Samsonite Painting & More" />
+            <meta name="keywords" content="pressure washing, roof cleaning, gutter cleaning, painting services, roof cleaning, algae removal, moss cleaning, roof maintenance, Little Rock, Benton, Bryant, Searcy, Conway, Arkansas, Pressure Washing near me, Roof Cleaning near me, Gutter Cleaning near me, Painting Services near me, Algae Removal near me, Moss Cleaning near me, Roof Maintenance near me, Pressure Washing Little Rock, Roof Cleaning Little Rock, Gutter Cleaning Little Rock, Painting Services Little Rock, Algae Removal Little Rock, Moss Cleaning Little Rock, Roof Maintenance Little Rock, Pressure Washing Conway, Roof Cleaning Conway, Gutter Cleaning Conway, Painting Services Conway, Algae Removal Conway, Moss Cleaning Conway, Roof Maintenance Conway, Pressure Washing Benton, Roof Cleaning Benton, Gutter Cleaning Benton, Painting Services Benton, Algae Removal Benton, Moss Cleaning Benton, Roof Maintenance Benton, Pressure Washing Bryant, Painting Services Bryant, Roof Cleaning Bryant, Gutter Cleaning Bryant, Roof Maintenance Bryant, Pressure Washing Searcy, Roof Cleaning Searcy, Gutter Cleaning Searcy, Painting Services Searcy, Roof Maintenance Searcy, Commercial Pressure Washing, Commercial Roof Cleaning, Commercial Gutter Cleaning, Commercial Painting Services, Commercial Roof Maintenance, Commercial Roof Cleaning Little Rock, Commercial Roof Cleaning Conway, Commercial Roof Cleaning Benton, Commercial Roof Cleaning Bryant, Commercial Roof Cleaning Searcy, Commercial Roof Cleaning Little Rock, Commercial Roof Cleaning Conway, Commercial Roof Cleaning Benton, Commercial Roof Cleaning Bryant, Commercial Roof Cleaning Searcy, Commercial Roof Cleaning Little Rock, Commercial Roof Cleaning Conway, Commercial Roof Cleaning Benton, Commercial Roof Cleaning Bryant, Commercial Roof Cleaning Searcy, Concrete Pressure Washing, Concrete Cleaning, Concrete Painting, Concrete Cleaning Little Rock, Concrete Cleaning Conway, Concrete Cleaning Benton, Concrete Cleaning Bryant, Concrete Cleaning Searcy, Graffitti Cleaning, Graffiti Cleaning Little Rock, Graffiti Cleaning Conway, Graffiti Cleaning Benton, Graffiti Cleaning Bryant, Graffiti Cleaning Searcy, window cleaning, window cleaning Little Rock, window cleaning Conway, window cleaning Benton, window cleaning Bryant, window cleaning Searcy, window washing, window washing Little Rock, window washing Conway, window washing Benton, window washing Bryant, window washing Searcy" />
+            <meta
+            name="description"
+            content="Professional pressure washing and painting services in Central Arkansas, including Little Rock, Benton, Bryant, Searcy, and Conway."
+            />
+            <meta property="og:site_name" content="Samsonite Painting & More" />
+            <meta property="og:url" content="https://spmpsolutions.com/" />
+            <meta property="og:type" content="website" />
+            <link rel="canonical" href="https://spmpsolutions.com/" />
+            <link rel="shortcut icon" href="/assets/img/faviconSam.png" />
+            <link rel="preconnect" href="https://fonts.bunny.net" />
+            <link href="https://fonts.bunny.net/css?family=dm-sans:400,400i,500,500i,700,700i|poppins:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet" />
+            <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+                __html: JSON.stringify({
+                "@context": "https://schema.org",
+                "@type": "LocalBusiness",
+                "name": "Samsonite Painting & More",
+                "address": {
+                    "@type": "PostalAddress",
+                    "addressLocality": "Little Rock",
+                    "addressRegion": "AR",
+                    "postalCode": "72201"
+                },
+                "telephone": "501-533-9213",
+                "url": "https://spmpsolutions.com"
+                }),
+            }}
+            />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
